d57562e62b27e6d0d16811cc0f864f86b0d50a68
David Herrero <dhd0004@alu.ubu.es>
2012-03-19 17:54:42 +0100

    #7 prototipo interfaz

--- Prototipos/PrototipoSSH/gen/ubu/inf/prototipos/R.java
+++ Prototipos/PrototipoSSH/gen/ubu/inf/prototipos/R.java
@@ -42,6 +42,7 @@ public final class R {
         public static final int hola=0x7f030000;
         public static final int loginssh=0x7f030001;
         public static final int main=0x7f030002;
+        public static final int tabssh=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
